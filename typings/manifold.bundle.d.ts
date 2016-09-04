5dabe494e00e6570585d1471060dd5ecd73174aa
edsilv <e.silverton@gmail.com>
2016-09-04 16:40:12 +0100

    improved config structure

--- typings/manifold.bundle.d.ts
+++ typings/manifold.bundle.d.ts
@@ -1,36 +1,3 @@
-// base-component v1.0.1 https://github.com/viewdir/base-component#readme
-interface Window {
-    _Components: any;
-}
-
-declare var TinyEmitter: any;
-declare namespace _Components {
-    class BaseComponent implements IBaseComponent {
-        options: IBaseComponentOptions;
-        protected _$element: JQuery;
-        constructor(options: IBaseComponentOptions);
-        protected _init(): boolean;
-        protected _getDefaultOptions(): IBaseComponentOptions;
-        protected _emit(event: string, ...args: any[]): void;
-        protected _resize(): void;
-        databind(data: any): void;
-    }
-    function applyMixins(derivedCtor: any, baseCtors: any[]): void;
-}
-
-declare namespace _Components {
-    interface IBaseComponent {
-        options: IBaseComponentOptions;
-        databind(data: any): void;
-    }
-}
-
-declare namespace _Components {
-    interface IBaseComponentOptions {
-        element?: string;
-    }
-}
-
 // manifesto.js v0.1.26 https://github.com/universalviewer/manifesto
 declare module exjs {
     var version: string;
@@ -1537,36 +1504,5 @@ declare namespace Manifold {
         labels: Object;
         constructor(labels: Object);
         format(url: any): string;
-    }
-}
-
-// custom definitions go in here
-declare namespace IIIFComponents {
-    interface ISvgDrawComponent extends _Components.IBaseComponent {
-    }
-}
-
-declare namespace IIIFComponents {
-    interface ISvgDrawComponentOptions extends _Components.IBaseComponentOptions {
-        overlayType?: string;
-    }
-}
-
-declare namespace IIIFComponents {
-    class SvgDrawComponent extends _Components.BaseComponent implements ISvgDrawComponent {
-        options: ISvgDrawComponentOptions;
-        private _$canvas;
-        constructor(options: ISvgDrawComponentOptions);
-        debug(msg: any): void;
-        addPoint(point: any): void;
-        protected _init(): boolean;
-        protected _getDefaultOptions(): ISvgDrawComponentOptions;
-        protected _resize(): void;
-    }
-}
-declare namespace IIIFComponents.SvgDrawComponent {
-    class Events {
-        static DEBUG: string;
-        static ADDPOINT: string;
     }
 }
